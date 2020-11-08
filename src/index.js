e90fd994eba6794e6d893b6be785103bd70a7353
ankitjain28may <ankitjain28may77@gmail.com>
2020-11-08 23:22:56 +0530

    Added list files in PR github action

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const { Octokit } = require("@octokit/core");
+const { context } = require("@actions/github");
+const { setOutput, setFailed } = require("@actions/core");
+const inputHelper = require("./input-helper");
+
+const QUERY = `query($cursor:String, $owner: String!, $repo: String!, $pr_number: Int!)
+{
+  repository(name: $repo, owner: $owner) {
+    pullRequest(number: $pr_number) {
+      files(first: 100, after:$cursor) {
+        nodes {
+          path
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+}
+`;
+
+if (context.eventName != "pull_request") {
+  throw new Error(
+    `This is currently enabled for 'pull_request' type of events`
+  );
+}
+
+const params = {
+  owner: inputHelper.owner,
+  repo: inputHelper.repo,
+  pr_number: inputHelper.pull_request_no,
+};
+
+
+const pullRequestFiles = [];
+let pullRequestFilesFormat = "";
+const octokit = new Octokit(inputHelper.github);
+
+async function fetchFilesFromPR(octokit, { results, cursor } = { results: [] }) {
+  params.cursor = cursor
+  const { repository: { pullRequest }} = await octokit.graphql(QUERY, params);
+  results.push(...pullRequest.files.nodes);
+  if (pullRequest.files.pageInfo.hasNextPage) {
+    await fetchFilesFromPR(octokit, {
+      results,
+      cursor: pullRequest.files.pageInfo.endCursor,
+    });
+  }
+  return results;
+}
+
+fetchFilesFromPR(octokit)
+  .then(function (result) {
+    result.forEach((element) => {
+      pullRequestFiles.push(element.path);
+    });
+
+    switch (inputHelper.outputFormat) {
+      case "space-delimited":
+        pullRequestFilesFormat = pullRequestFiles.join(" ");
+        break;
+      case "csv":
+        pullRequestFilesFormat = pullRequestFiles.join(",");
+        break;
+      case "json":
+        pullRequestFilesFormat = JSON.stringify(pullRequestFiles);
+        break;
+    }
+    setOutput("pullRequestFiles", pullRequestFilesFormat);
+  })
+  .catch((error) => {
+    setFailed(error);
+  });
---
+++ src/input-helper.js
@@ -0,0 +1,40 @@
+const { getInput } = require("@actions/core");
+const { context } = require("@actions/github");
+
+function getInputs() {
+  const result = {
+    github: {},
+  };
+
+  // Github Token
+  result.github.auth =
+    getInput("githubToken") || process.env.GITHUB_TOKEN || false;
+  if (!result.github.auth) {
+    throw new Error("Github Token not found");
+  }
+
+  result.outputFormat = getInput("outputFormat") || "json";
+  if (
+    result.outputFormat != "space-delimited" &&
+    result.outputFormat != "json" &&
+    result.outputFormat != "csv"
+  ) {
+    throw new Error(
+      "Output Format should be one of the following: space-delimited, json, csv"
+    );
+  }
+  // Github baseURL
+  if (getInput("baseUrl")) {
+    result.github.baseUrl = getInput("baseUrl");
+  }
+  // Github Repo Owner
+  result.owner = context.repo.owner;
+  // Github Repo name
+  result.repo = context.repo.repo;
+  // Github Pull Request Number
+  result.pull_request_no = getInput("pullRequestNo") || context.issue.number;
+
+  return result;
+}
+
+module.exports = getInputs();
